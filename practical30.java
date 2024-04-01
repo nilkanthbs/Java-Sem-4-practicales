79cba8b908d6a79dc84bad9c87594af2e3bd7df4
nilkanthbs <nilkanthsavaliya1725@gmail.com>
2024-04-01 09:29:32 +0530

    practical30 complete

---
+++ practical30.java
@@ -0,0 +1,18 @@
+
+public class Program30_Wrapper {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println("Value of a = " + a);
+
+        Integer i = Integer.valueOf(a);
+        System.out.println("Object Value after autoboxing = " + i);
+
+        // Float f = Float.valueOf(a);
+        // System.out.println(f);
+        // String str = String.valueOf(a);
+        // System.out.println(str);
+
+        int b = i.intValue();
+        System.out.println("Primitive Value after unboxing = " + b);
+    }
+}
