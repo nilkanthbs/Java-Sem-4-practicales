02063b02adfbf38711ec7c5c324dd88c721fa236
nilkanthbs <nilkanthsavaliya1725@gmail.com>
2024-04-01 09:33:41 +0530

    31 complete

---
+++ practical31.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class practical31 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a, b;
+
+        System.out.println("Enter 1st Number :-");
+        a = sc.nextInt();
+        System.out.println("Enter 2nd Number :-");
+        b = sc.nextInt();
+        sc.close();
+
+        System.out.println("(a,b) = (" + a + "," + b + ")");
+
+        System.out.println("Maximum of (a,b) = " + Math.max(a, b));
+        System.out.println("Square Root of a = " + Math.sqrt(a));
+        System.out.println("Sine of a =" + Math.sin(a));
+        System.out.println("Cos of b = " + Math.cos(b));
+        System.out.println(a + " raised to power " + b + " = " + Math.pow(a, b));
+    }
+}
